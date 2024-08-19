6b6afdf3bca4686332aaa3a9d5f39015df5cbaa5
lebedev-023046 <lebedev.023046@gmail.com>
2024-08-19 22:14:11 +0300

    feat: create select component

--- js/index.js
+++ js/index.js
@@ -8,3 +8,31 @@ menuItemsBlock.addEventListener('click', (event) => {
     checkbox.checked = !checkbox.checked;
   }
 });
+
+// SELECT
+
+const alllangs = ['ru', 'en'];
+
+const select = document.querySelector('#lang-select-id');
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+  let lang = select.value;
+  console.log(lang);
+  location.href = `${window.location.pathname}#${lang}`;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash.substring(1);
+
+  if (!alllangs.includes(hash)) {
+    location.href = `${window.location.pathname}#ru`;
+    location.reload();
+  }
+
+  select.value = hash;
+}
+
+changeLanguage();
